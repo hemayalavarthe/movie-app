a36eaa46d2bdd7d7f2db9b27c4dcf7d5a39271b0
Hema Yalavarthi <132774002+hemayalavarthe@users.noreply.github.com>
2024-06-02 22:37:55 +0530

    Update navbar.js

--- component/navbar.js
+++ component/navbar.js
@@ -1,5 +1,14 @@
 function navbar() {
   return `
+    <div id="one">
+    <div id="list"><i class="fa-solid fa-bars fa-1x" ></i></div>
+    <div id="logo"><a href="index.html"><img src="https://secure-media.hotstarext.com/web-assets/prod/images/brand-logos/disney-hotstar-logo-dark.svg" alt=""></a></div>
+    <div>TV</div>
+    <div>Movies</div>
+    <div>Sports</div>
+    <div>Disney+</div>
+    <div id="kid"></div>
+</div>
 
 <div id="two">
     <div id="first"><input type="text"><i class="fa-solidfa-magnifying-glass"></i></div>
@@ -15,12 +24,4 @@ export default navbar
 
 
 
-//   <div id="one">
-//     <div id="list"><i class="fa-solid fa-bars fa-1x" ></i></div>
-//     <div id="logo"><a href="index.html"><img src="https://secure-media.hotstarext.com/web-assets/prod/images/brand-logos/disney-hotstar-logo-dark.svg" alt=""></a></div>
-//     <div>TV</div>
-//     <div>Movies</div>
-//     <div>Sports</div>
-//     <div>Disney+</div>
-//     <div id="kid"><img src="kidlogo.png" alt=""></div>
-// </div>
+
